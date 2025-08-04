3b1bb310c5d0fd6abc87ecc0b4d7a124e43552ce
kkyung99 <becky0508@naver.com>
2025-08-04 22:07:18 +0900

    feat: MainPage에 사이드바 컴포넌트 추가 및 시맨틱 태그 구조 개선

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,21 +1,25 @@
+import Sidebar from '../components/main/Sidebar';
+
 export default function MainPage() {
   return (
-    <div className="w-screen flex pt-[30px]">
+    <main className="w-screen flex pt-[30px]">
       <div className="max-w-[1280px] mx-auto flex relative">
-        <div className="flex gap-5 w-[1000px] bg-blue-100 px-[30px] pt-[30px]">
-          <div className="flex flex-col w-1/3 gap-3.5">
-            <div className="h-[500px] bg-blue-300">Box1</div>
-            <div className="h-[300px] bg-blue-400">Box4</div>
-          </div>
-          <div className="flex flex-col w-2/3 gap-3.5">
-            <div className="h-[600px] bg-blue-500">Box2</div>
-            <div className="h-[220px] bg-blue-600">Box5</div>
-          </div>
-        </div>
-        <div className="w-[250px] h-[500px] bg-blue-200 ml-[30px] absolute right-[-300px] ">
-          Box3
-        </div>
+        <section className="flex gap-5 w-[1000px] bg-blue-100 px-[30px] pt-[30px]">
+          <aside className="flex flex-col w-1/3 gap-3.5">
+            <article className="h-[500px] bg-blue-300">Box1</article>
+            <article className="h-[300px] bg-blue-400">Box4</article>
+          </aside>
+
+          <section className="flex flex-col w-2/3 gap-3.5">
+            <article className="h-[600px] bg-blue-500">Box2</article>
+            <article className="h-[220px] bg-blue-600">Box5</article>
+          </section>
+        </section>
+
+        <aside className="w-[300px] ml-[30px]">
+          <Sidebar />
+        </aside>
       </div>
-    </div>
+    </main>
   );
 }
